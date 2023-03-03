9c6b1d8d7a307d24f622457a888e130b4ad13e03
Mark Yen <mark11yan@gmail.com>
2023-03-02 23:31:49 -0800

    Update app.py

--- examples/flask-kitchensink/app.py
+++ examples/flask-kitchensink/app.py
@@ -118,7 +118,7 @@ def handle_text_message(event):
                 event.reply_token,
                 TextSendMessage(text="Bot can't use profile API without user ID"))
     elif text == 'Joke':
-        r_json = requests.post("https://meme-api.com/gimme/me_irl").json()
+        r_json = requests.get("https://meme-api.com/gimme/me_irl").json()
         text_message = ImageSendMessage(original_content_url=r_json.get("url"), preview_image_url=r_json.get("preview")[-1],)
         line_bot_api.reply_message(
             event.reply_token, [
